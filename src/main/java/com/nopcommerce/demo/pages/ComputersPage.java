4d876792aa8fef41201b6baea84f861fa08717fb
rutvisha <rutvishapatel9@gmail.com>
2023-12-05 20:48:46 +0000

    rutvisha

---
+++ src/main/java/com/nopcommerce/demo/pages/ComputersPage.java
@@ -0,0 +1,20 @@
+package com.nopcommerce.demo.pages;
+
+import com.nopcommerce.demo.utilities.Utility;
+import org.openqa.selenium.By;
+
+public class ComputersPage extends Utility {
+    By welcomeText = By.xpath("//h1[normalize-space()='Computers']");
+    By desktopsMenuLink = By.xpath("//li[@class='active last']//a[normalize-space()='Desktops']");
+
+    /******************************** All Elements' Action Methods on Computers page *************************************/
+
+    public String getWelcomeText() {
+        return getTextFromElement(welcomeText);
+    }
+
+    public void clickOnDesktopsMenu () {
+        clickOnElement(desktopsMenuLink);
+    }
+
+}
